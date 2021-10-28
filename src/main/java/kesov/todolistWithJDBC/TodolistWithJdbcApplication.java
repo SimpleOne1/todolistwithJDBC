f099b60f856bb6e974b531f872a0ad8e76e29d88
Данил <dan.kesov.2015@gmail.com>
2021-10-28 22:26:56 +0300

    init-commit

---
+++ src/main/java/kesov/todolistWithJDBC/TodolistWithJdbcApplication.java
@@ -0,0 +1,13 @@
+package kesov.todolistWithJDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistWithJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistWithJdbcApplication.class, args);
+	}
+
+}
